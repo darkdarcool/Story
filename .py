58fb7a0625c13424671f54cfcda60fd4baabb6b2
darkdarcool <66882633+darkdarcool@users.noreply.github.com>
2020-10-15 11:48:47 -0700

    Create .py

---
+++ .py
@@ -0,0 +1,38 @@
+import os,random
+import time, sys
+
+
+#colors
+
+black = "\033[0;30m"
+red = "\033[0;31m"
+green = "\033[0;32m"
+yellow = "\033[0;33m"
+blue = "\033[0;34m"
+magenta = "\033[0;35m"
+cyan = "\033[0;36m"
+white = "\033[0;37m"
+bblack = "\033[0;90m"
+bred = "\033[0;91m"
+bgreen = "\033[0;92m"
+byellow = "\033[0;93m"
+bblue = "\033[0;94m"
+bmagenta = "\033[0;95m"
+bcyan = "\033[0;96m"
+bwhite = "\033[0;97m"
+
+
+message = (bgreen + "LOADING\n")
+
+
+
+def typewriter(message):
+	for i in message:
+		sys.stdout.write(i)
+		sys.stdout.flush()
+		if ((i != "\n") and (i != ":")):
+			time.sleep(0.09)
+		else:
+			time.sleep(0.6)
+
+typewriter(red + 'YO')
