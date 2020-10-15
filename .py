e7997432372d3762fabc291ca22e484f23b13bd7
Darren Reddy <66882633+darkdarcool@users.noreply.github.com>
2020-10-15 15:30:03 -0700

    Update .py
    Change sp to typewriter!!!!

--- .py
+++ .py
@@ -239,3 +239,97 @@ print('OH, your on!!')
 time.sleep(2)
 os.system('clear')
 typewriter(bcyan + 'You play a couple COD matches with Justin until you have to go to dinner. When you sre putting everything away, you see a box that looks like an old gameboy. You put it in your school backpack for the bus in case your phone died. The you ran downstairs for dinner. ')
+def drawer2():
+  typewriter("You look at a drawer. It seems like an important drawer. Open it?\n (Y/N) ")
+  openit = input('   >>>>>>>    ').upper()
+  openit = openit.upper()
+  if openit == "Y":
+      clr()
+      typewriter("It seems... like it has a note. It says...")
+      time.sleep(3)
+      clr()
+      typewriter("Mz3U44xNjIga3434XM33g3d3G4334433334h4lIGNv45Z4G3Uu7")
+      print()
+      typewriter("\nYou observe it for 15 seconds.")
+      time.sleep(15)
+      clr()
+      typewriter("You see a tablet under. Enter a passcode, please. ")
+      typewriter('{THERE ARE ONLY NUMBERS ON THIS}')
+      passcode = input('\n>>> ')
+      if passcode == "35162":
+         clr()
+         typewriter("You got 100 XP"
+           )
+         xp + 100 
+         time.sleep(3)
+         
+           
+
+
+      else:
+         clr()
+         typewriter("Huh, no open sesame?")
+         time.sleep(3)
+  elif openit == "N":
+    typewriter('It\'s probably nothing, let\'s just get going. ')
+    time.sleep(2.5)
+    os.system('clear')
+  closs()
+
+def closs():
+  time.sleep(0.5)
+  os.system('clear')
+  print('a) Drawer')
+  print('b) Try the computer again')
+  print('c) Try the mini fridge ')
+  print('d) Try the safe ')
+  print('e) Look at the notepad ')
+  print('f) Try the computer password')
+  dee = input('Which one?\n    >>>>  ').lower()
+  dee = dee.lower()
+  if dee == "a":
+    drawer2()
+    os.system('clear')
+  elif dee == "b":
+    sp('William tries the computer again, it will not let him in without a password.')
+    time.sleep(2.5)
+    os.system('clear')
+    closs()
+  elif dee == "c":
+    sp('When Willaim opens the fridge, on one bag, it says, {35162}\nIt seems important, so Willaim writes it down.')
+    time.sleep(2.5)
+    os.system('clear')
+    closs()
+  elif dee == 'd':
+    sp(' The safe needs a password, I should try it! ')
+    print('ENTER PASSCODE:')
+    doo = input('  >> ')
+    if doo == "35162":
+      spslo('PASSWORD ACCEPTED')
+      sp('Inside the safe is a page that reads (PAGE 347 OF NOTEBOOK)')
+      time.sleep(2.5)
+      os.system('clear')
+      closs()
+  elif dee == "e":
+    sp('The notepad has many pages, but almost all of them have nothing on them, out of 657 pages, which one do you turn to?')
+    dooo = input('  >>>>>>    ')
+    if dooo == "347":
+      sp('You flip to page ' + dooo + ' and you find the computer passcode: ThisIsStupid')
+      time.sleep(2.5)
+      os.system('clear')
+      closs()
+    else:
+      sp('You flip to page ' + dooo + ' and see nothing. Well, better go find the right number!')
+      time.sleep(2.5)
+      os.system('clear')
+    closs()
+  elif dee == "f":
+    sp('PASSCODE: ')
+    passw = input('>>>>>>   ')
+    if passw == "ThisIsStupid":
+      sp(' The computer let William In!\n"YESSSSSSSS!!!" Willaim screames')
+    else:
+      spslo('DENIED')
+      time.sleep('2.1')
+      os.system('clear')
+      closs()
